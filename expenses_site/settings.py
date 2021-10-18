8c5812f626e74ae96fad3a58f5ffc4906ad3fd3b
Sarah <sarah.ali2810@gmail.com>
2021-10-19 01:28:56 +0200

    fixing

--- expenses_site/settings.py
+++ expenses_site/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os ,sys, platform
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -145,5 +146,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Configure Django App for Heroku.
-import django_heroku
+
 django_heroku.settings(locals())
