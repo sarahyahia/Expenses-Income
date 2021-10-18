13d8711b750bdba5c22473a5eade4feba123d42b
Sarah <sarah.ali2810@gmail.com>
2021-10-18 22:02:54 +0200

    setup env, templates, static files

---
+++ authentication/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, redirect
+from django.views import View
+
+
+class RegistrationView(View):
+    def get(self, request):
+        return render(request, 'authentication/register.html')
+
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'authentication/login.html')
+    
+
+
